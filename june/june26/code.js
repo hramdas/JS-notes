4f1be7a8257ce417d27e637857bdd1adc45e1e02
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-06-26 21:15:58 +0530

    JS25 june

---
+++ june/june26/code.js
@@ -0,0 +1,21 @@
+function runProgram(input){
+    var a = Number(input);
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+   read += input;
+});
+process.stdin.on("end", function () {
+   read = read.replace(/\n$/,"")
+  runProgram(read);
+});
+
+process.on("SIGINT", function () {
+   read = read.replace(/\n$/,"")
+   runProgram(read);
+   process.exit(0);
+});
