d47f6873119b81c4ef407de7a6674a08f7ea62fc
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-11-08 17:10:25 +0530

    as1

--- July/july13/as1.js
+++ July/july13/as1.js
@@ -1,31 +1,10 @@
 var arr = [2, 4, 5, 3, 6, 8];
 
-var index = function(el, i){
-    return i
+var res = []
+for(var i=0; i<arr.length; i++){
+    if(i%2==0 && arr[i]%2 == 0){
+        res.push(arr[i])
+    }
 }
+console.log(res)
 
-// var even = function(el){
-//     for(var i = 2; i < arr.length; i++)
-//     count = 0
-//     if(el/i == 0){
-//         count++
-//     }
-//     if(count==0){
-//         return el
-//     }
-// }
-
-// out = arr.map(index).reduce(even)
-// console.log(out)
-
-el = arr
-
-count = 0
-for(var i = 2; i < el.length; i++){
-    
-    if(el/i == 0)
-        count++
-    
-    if(count==0)
-        console.log(el)
-    
